6223152219da582578266d6ffd4d04e2a2963886
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2021-11-30 12:45:56 +0100

    Aufgabe 1,2

---
+++ Aufgaben_Namen_Multikplikation.py
@@ -0,0 +1,10 @@
+#Aufgabe 1
+vorname = input("Vorname")
+nachname = input("Nachname")
+
+print(vorname , nachname)
+
+#Aufgabe2
+
+rechnung = int(input("1 Zahl")) * int(input("2 Zahl")) * int(input("3 Zahl"))
+print(rechnung)
