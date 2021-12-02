9d1546efd292977066cc4ef0ca43e5a85b293bac
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2021-12-02 10:50:00 +0100

    more

--- 01 Variablen/Aufgaben_Namen_Multikplikation.py
+++ 01 Variablen/Aufgaben_Namen_Multikplikation.py
@@ -4,6 +4,10 @@ nachname = input("Nachname")
 
 print(vorname , nachname)
 
+
+
+
+
 #Aufgabe2
 
 rechnung = int(input("1 Zahl")) * int(input("2 Zahl")) * int(input("3 Zahl"))
---
+++ 02 Kontrollstrukturen/Einfuehrung_Kontrollstruktur_mitPruefung.py
@@ -0,0 +1,8 @@
+zahl = int(input("Zahl: "))
+
+if zahl < 0:
+    print("Zahl ist kleiner 0")
+elif zahl > 0:
+    print("Zahl ist grösser als 0")
+elif zahl == 0:
+    print("Zahl ist gleich 0")
