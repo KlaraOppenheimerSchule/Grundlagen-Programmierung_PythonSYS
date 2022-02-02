28c4dbc6992de672474a809e8b38076ce69c333d
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2022-02-02 12:43:50 +0100

    Schleifen

---
+++ 04 Komplexe Datentypen/EinfuehrungListen.py
@@ -0,0 +1,5 @@
+essensWuensche= ["Nudel mit Tomatensosse", 2222, "Veg. Gyros", "Hähnchen mit Antibiotika", "Fisch"]
+tag=1
+for einzelEssen in essensWuensche:
+    print("Essen am Tag: " + str(tag) + " " + str(einzelEssen))
+    tag=tag+1
