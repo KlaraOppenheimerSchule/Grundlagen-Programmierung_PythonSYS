cbf072dc67ff4c26b646052927c450da4f0799d5
Christoph Zobel <zobel@klara-oppenheimer-schule.de>
2025-01-01 16:44:29 +0100

    Lotto

--- 04 Komplexe Datentypen/Einfuehrung_Gluecksrad.py
+++ 04 Komplexe Datentypen/Einfuehrung_Gluecksrad.py
@@ -1,7 +1,7 @@
 from random import randint
 
+#Sechs Zufallszahlen vom Computer erzeugen lassen
 zufallszahlen = []
-nutzerZahlen = []
 
 for x in range(0,6):
     zufallszahl = randint(1,42)
@@ -9,16 +9,21 @@ for x in range(0,6):
 
 print(zufallszahlen)
 
+
+#Sechs Zahlen vom Nutzer eingeben lassen
+nutzerZahlen = []
+
 for n in range(0,6):
     nutzerZahl = int(input("Zahl eingeben: "))
     nutzerZahlen.append(nutzerZahl)
 
+
+#Ermitteln, wie viele Zahlen richtig erraten wurden
 gleicheZahlen = []
  
 for x in nutzerZahlen:
     if x in zufallszahlen:
-        #doesnt work - to check
-        #if(x not in zufallszahlen):
+        if(x not in gleicheZahlen):
             gleicheZahlen.append(x)
  
 size =len(gleicheZahlen)
